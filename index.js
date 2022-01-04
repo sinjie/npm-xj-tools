573fd108a91d0890321d69b2323574d107a0b70e
星孑 <min281161191@163.com>
2022-01-04 17:27:50 +0800

    feat(批量引入)

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+const fs = require('fs')
 // 批量引入data目录下的js文件 并导出
-const modulesFiles = require.context('./tools/', true, /\.js$/)
-const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-  const value = modulesFiles(modulePath)
-  return { ...modules, ...value }
-}, {})
+const modules = fs.readdirSync('./tools').map(item => {
+  return require(`./${item}`)
+})
 
 module.exports = modules
-
