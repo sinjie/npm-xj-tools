6ce04acb75542e69407c458b746ea0e940f38687
星孑 <min281161191@163.com>
2022-01-04 16:58:56 +0800

    build

--- tools/array.js
+++ tools/array.js
@@ -4,7 +4,7 @@
  * @param {Array ...} 多个数组传入
  * @returns {Array} 新数组
  */
-export const mergeArray = () => {
+export function mergeArray() {
   var arr = Array.prototype.slice.apply(arguments)
   arr = arr.reduce((x, y) => {
     return x.concat(y)
@@ -17,7 +17,7 @@ export const mergeArray = () => {
  * @param {Array ...} 多个数组传入，仅限单层数组
  * @returns {Array} 新数组
  */
-export const mergeRemoveSame = () => {
+export function mergeRemoveSame() {
   var arr = Array.prototype.slice.apply(arguments)
 
   arr = arr.reduce((x, y) => {
@@ -36,7 +36,7 @@ export const mergeRemoveSame = () => {
  * @param {Array ...} 多个数组传入，仅限单层数组
  * @returns {Array} 新数组
  */
-export const mergeGetOnly = () => {
+export function mergeGetOnly() {
   var arr = Array.prototype.slice.apply(arguments)
   arr = arr.reduce((x, y) => {
     return x.concat(y)
